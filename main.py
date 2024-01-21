eb2a9da7d8227f7c8a48aa21ee238a7b28155fc7
RaidBan <58857553+RaidBan@users.noreply.github.com>
2024-01-21 19:13:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, request, reqparse
+import json
 
 app = Flask(__name__)
 api = Api()
 
-courses = {
-        1:{"name": "Python", "videos": 15},
-        2:{"name": "Java", "videos": 6}
-        }
+
+def jsonin():
+    with open("courses.json", "r") as raw:
+        courses = json.load(raw)
+    return courses
+
+
+def jsonout(out):
+    with open("courses.json", "w") as done:
+        json.dump(out, done, ensure_ascii=False, indent=4)
+
+
+course = jsonin()
+
+
 class Main(Resource):
     def get(self, course_id):
-        if course_id == 0:
-            return courses
+        if course_id == '0':
+            return course
         else:
-            return courses[course_id]
+            return course[course_id]
 
     def delete(self, course_id):
-        del courses[course_id]
-        return courses
+        del course[course_id]
+        jsonout(course)
+        return course
 
     def post(self, course_id):
-        par = reqparse.RequestParser()
-        par.add_argument("name")
-        par.add_argument("videos")
-        courses[course_id] = par.parse_args()
-        return courses
+        try:
+            par = reqparse.RequestParser()
+            par.add_argument("name", type=str)
+            par.add_argument("videos", type=str)
+            req = par.parse_args()
+            course[course_id] = req
+            jsonout(course)
+            return req
+        except:
+            req = request.args
+            course[course_id] = req
+            jsonout(course)
+            return req
 
 
-api.add_resource(Main, "/api/main/<int:course_id>")
+api.add_resource(Main, "/api/main/<course_id>")
 api.init_app(app)
 
 if __name__ == "__main__":
